ffbfc76b174234285463440de853a356affdc767
Kim-Yiji <yiji_kim@yonsei.ac.kr>
2025-06-18 20:58:47 +0900

    delete : debug html 삭제:

--- scripts/main.js
+++ scripts/main.js
@@ -69,7 +69,7 @@ window.addEventListener('DOMContentLoaded', () => {
   sceneManager.renderLoop();
 
   // 6) 최초 씬 설정
-  sceneManager.transitionTo('resultBroadcast');
+  sceneManager.transitionTo('home');
 
   // 7) 창 크기 변화 처리
   window.addEventListener('resize', () => {
